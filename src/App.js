9dc69f8727260819358a3cef7c8075b097c10d6c
saich <saich@LAPTOP-8Q9AR8V0>
2023-03-02 18:37:50 +0530

    new commits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./normal.css";
+import { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [chatLog, setChatLog] = useState([]);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setChatLog([...chatLog, { user: "me", message: `${input}` }]);
+    console.log("submit");
+    setInput("");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <aside className="sidemenu">
+        <div className="side-menu-button">
+          <span>+</span>
+          New Chat
+        </div>
+      </aside>
+      <section className="chatbox">
+        <div className="chat-log">
+          <ChatMessage message={message} />
+          <div className="chat-message chatgpt">
+            <div className="chat-message-center">
+              <div className="avatar chatgpt"></div>
+              <div className="message">I am an AI</div>
+            </div>
+          </div>
+        </div>
+        <div className="chat-input-holder">
+          <form onSubmit={handleSubmit}>
+            <input
+              rows="1"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              className="chat-input-textarea"
+            />
+          </form>
+        </div>
+      </section>
     </div>
   );
 }
-
+const ChatMessage = ({ message }) => {
+  return (
+    <div className="chat-message">
+      <div className="chat-message-center">
+        <div className="avatar"></div>
+        <div className="message">{message.message}</div>
+      </div>
+    </div>
+  );
+};
 export default App;
